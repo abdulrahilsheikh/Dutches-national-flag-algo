291a5c52edb9abcd6a9963fde33571d07de73881
Rahil Sheikh <rahil.sheikh@coditas.com>
2023-05-22 14:57:26 +0530

    Added changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import DNF from "./components/dutches-national-flag/DNF";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="w-full h-screen flex items-center justify-center bg-[#262624]">
 			<DNF />
